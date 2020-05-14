2cbe7490df2d6a02b2d589841955dddfb2fa3f58
tinker0830 <erin23145@gmail.com>
2020-05-14 22:32:40 +0530

    jobs fetched on frontend

--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
+const cors=require('cors')
 const redis = require("redis");
 const client = redis.createClient();
 const { promisify } = require("util");
 const getAsync = promisify(client.get).bind(client);
 const port = 4000;
 
+app.use(cors());
 app.get('/jobs', async(req, res) =>{
+
     const jobs=await getAsync('github');
     console.log(JSON.parse(jobs).length);
-    return res.send('Hello World!')
+    return res.send(jobs)
+
 }); 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Jobs from './components/Jobs';
 
 import './App.css';
+const JOB_API_URI='http://localhost:4000/jobs'
 
 const mockJobs=[
   {title:'SWE3',company:'Apple'},
@@ -9,10 +10,21 @@ const mockJobs=[
   {title:'SWE4',company:'Apple'}
   
 ]
+
+async function fetchJobs(updatedCb){
+  const res=await fetch(JOB_API_URI);
+  const json=await res.json();
+  updatedCb(json)
+  console.log(json);
+}
 function App() {
+  const [jobList,updatedJobs]=React.useState([]);
+  React.useEffect(()=>{
+    fetchJobs(updatedJobs);
+  },[])
   return (
     <div className="App">
-      <Jobs jobs={mockJobs}/>
+      <Jobs jobs={jobList}/>
     </div>
   );
 }
